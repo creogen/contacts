b3d0015b492ce7074929ec1c31bdef37d892ec22
Dmitry Merkushin <merkushin@gmail.com>
2022-03-27 13:45:50 +0300

    Register custom post type for contacts

--- src/PostType/ContactPostType.php
+++ src/PostType/ContactPostType.php
@@ -18,7 +18,7 @@ class ContactPostType implements PostTypeInterface {
 
 	public function __construct( Hooks $hooks_api, PostTypes $post_types_api ) {
 		$this->hooks_api = $hooks_api;
-		$this->post_types_api;
+		$this->post_types_api = $post_types_api;
 	}
 
 	public function register() {
@@ -26,8 +26,53 @@ class ContactPostType implements PostTypeInterface {
 	}
 
 	public function register_post_type() {
-		$args = [];
-		$this->post_types_api->register_post_type( 'creogen_contacts', $args );
+		$args = [
+			'labels' => [
+				'name' => 'Contacts Directory',
+				'singular_name' => 'Contact',
+				'add_new' => 'Add Contact',
+				'add_new_item' => 'Add Contact',
+				'edit' => 'Edit', 
+				'edit_item' => 'Edit Contact',
+				'view_item' => 'View Contact',
+				'search_items' => 'Search Contacts',
+				'not_found' => 'Contact Not Found',
+				'not_found_in_trash' => 'Contact Not Found in Trash',
+			],
+			'description' => 'Contacts directory on the website',
+			'public' => true,
+			'exclude_from_search' => false,
+			'publicly_queryable' => true,
+			'show_ui' => true,
+			'show_in_nav_menus' => false,
+			'show_in_menu' => true,
+			'show_in_admin_bar' => true,
+			'menu_position' => 20,
+			'menu_icon' => 'dashicons-clipboard',
+			'can_export' => true,
+			'capability_type' => 'post',
+			'map_meta_cap' => true,
+			'hierarchical' => false,
+			'supports' => [
+				'title', 
+				'thumbnail', 
+				'excerpt',
+			],
+			'taxonomies' => [],
+			'has_archive' => true,
+			'permalink_epmask' => 'EP_PERMALINK',
+			'rewrite' => [
+				'slug' => 'contacts',
+				'with_front' => true,
+				'feeds' => true,
+				'pages' => true,
+				'ep_mask' => 'EP_PERMALINK'
+			],
+			'query_var' => true,
+			'can_export' => true,
+			'show_in_rest' => true,
+		];
+		$this->post_types_api->register_post_type( 'creogen_contact', $args );
 	}
 }
 
--- tests/PostType/ContactPostTypeTest.php
+++ tests/PostType/ContactPostTypeTest.php
@@ -20,5 +20,64 @@ class ContactPostTypeTest extends TestCase {
 			->with( 'init', [ $contact_post_type, 'register_post_type' ] );
 		$contact_post_type->register();
 	}
+
+	public function testRegisterPostType_WhenCalled_CallsWpRegisterPostType() {
+		$hooks_api = $this->createMock( Hooks::class );
+		$post_types_api = $this->createMock( PostTypes::class );
+		$contact_post_type = new ContactPostType( $hooks_api, $post_types_api );
+
+		$expected_args = [
+			'labels' => [
+				'name' => 'Contacts Directory',
+				'singular_name' => 'Contact',
+				'add_new' => 'Add Contact',
+				'add_new_item' => 'Add Contact',
+				'edit' => 'Edit', 
+				'edit_item' => 'Edit Contact',
+				'view_item' => 'View Contact',
+				'search_items' => 'Search Contacts',
+				'not_found' => 'Contact Not Found',
+				'not_found_in_trash' => 'Contact Not Found in Trash',
+			],
+			'description' => 'Contacts directory on the website',
+			'public' => true,
+			'exclude_from_search' => false,
+			'publicly_queryable' => true,
+			'show_ui' => true,
+			'show_in_nav_menus' => false,
+			'show_in_menu' => true,
+			'show_in_admin_bar' => true,
+			'menu_position' => 20,
+			'menu_icon' => 'dashicons-clipboard',
+			'can_export' => true,
+			'capability_type' => 'post',
+			'map_meta_cap' => true,
+			'hierarchical' => false,
+			'supports' => [
+				'title', 
+				'thumbnail', 
+				'excerpt',
+			],
+			'taxonomies' => [],
+			'has_archive' => true,
+			'permalink_epmask' => 'EP_PERMALINK',
+			'rewrite' => [
+				'slug' => 'contacts',
+				'with_front' => true,
+				'feeds' => true,
+				'pages' => true,
+				'ep_mask' => 'EP_PERMALINK'
+			],
+			'query_var' => true,
+			'can_export' => true,
+			'show_in_rest' => true,
+		];
+
+		$post_types_api
+			->expects( self::once() )
+			->method( 'register_post_type' )
+			->with( 'creogen_contact', $expected_args );
+		$contact_post_type->register_post_type();
+	}
 }
 
