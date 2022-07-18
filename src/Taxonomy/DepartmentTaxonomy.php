1f4719524f28f32db102301baf2128fec0847344
Dmitry Merkushin <merkushin@gmail.com>
2022-07-18 22:26:42 +0300

    Register department taxonomy

--- src/Taxonomy/DepartmentTaxonomy.php
+++ src/Taxonomy/DepartmentTaxonomy.php
@@ -2,7 +2,9 @@
 
 namespace Creogen\Contacts\Taxonomy;
 
+use Creogen\Contacts\PostType\ContactPostType;
 use Merkushin\Wpal\Service\Hooks;
+use Merkushin\Wpal\Service\Taxonomies;
 use Merkushin\Wpal\ServiceFactory;
 
 class DepartmentTaxonomy implements TaxonomyInterface {
@@ -11,8 +13,14 @@ class DepartmentTaxonomy implements TaxonomyInterface {
 	 */
 	private $hooks_api;
 
+	/**
+	 * @var Taxonomies
+	 */
+	private $taxonomies;
+
 	public function __construct() {
 		$this->hooks_api = ServiceFactory::create_hooks();
+		$this->taxonomies = ServiceFactory::create_taxonomies();
 	}
 
 	public function register() {
@@ -20,7 +28,47 @@ class DepartmentTaxonomy implements TaxonomyInterface {
 	}
 
 	public function register_taxonomy() {
-
+		$this->taxonomies->register_taxonomy(
+			'taxonomy_department',
+			ContactPostType::get_post_type_name(),
+			[
+				'labels' => [
+					'name' => 'Departments',
+					'singular_name' => 'Department',
+					'search_items' => 'Search Departments',
+					'all_items' => 'All Departments',
+					'edit_item' => 'Edit Department',
+					'update_item' => 'Update Department',
+					'add_new_item' => 'Add New Department',
+					'new_item_name' => 'New Department Name',
+					'menu_name' => 'Departments',
+				],
+				'public' => true,
+				'hierarchical' => true,
+				'show_ui' => true,
+				'show_in_menu' => true,
+				'show_in_nav_menus' => false,
+				'show_tagcloud' => true,
+				'show_in_quick_edit' => true,
+				'meta_box_cb' => null,
+				'show_admin_column' => true,
+				'update_count_cb' => null,
+				'query_var' => 'taxonomy_department',
+				'rewrite' => [
+					'slug' => 'taxonomy_department',
+					'with_front' => true,
+					'hierarchical' => true,
+					'ep_mask' => 'EP_MASK',
+				],
+				'capabilities' => [
+					'manage_terms' => 'manage_categories',
+					'edit_terms' => 'manage_categories',
+					'delete_terms' => 'manage_categories',
+					'assign_terms' => 'edit_posts',
+				],
+				'sort' => true,
+			]
+		);
 	}
 }
 
--- tests/Taxonomy/DepartmentTaxonomyTest.php
+++ tests/Taxonomy/DepartmentTaxonomyTest.php
@@ -2,6 +2,7 @@
 
 use Creogen\Contacts\Taxonomy\DepartmentTaxonomy;
 use Merkushin\Wpal\Service\Hooks;
+use Merkushin\Wpal\Service\Taxonomies;
 use Merkushin\Wpal\ServiceFactory;
 use PHPUnit\Framework\TestCase;
 
@@ -13,6 +14,8 @@ class DepartmentTaxonomyTest extends TestCase {
 	public function testRegister_WhenCalled_AddsActionOnInit() {
 		$hooks = $this->createMock( Hooks::class );
 		ServiceFactory::set_custom_hooks( $hooks );
+		$taxonomies = $this->createMock( Taxonomies::class );
+		ServiceFactory::set_custom_taxonomies( $taxonomies );
 
 		$taxonomy = new DepartmentTaxonomy();
 
@@ -21,6 +24,61 @@ class DepartmentTaxonomyTest extends TestCase {
 			->method( 'add_action' )
 			->with( 'init', [ $taxonomy, 'register_taxonomy' ] );
 		$taxonomy->register();
+	}
+
+	public function testRegisterTaxonomy_WhenCalled_CallsWpRegisterTaxonomy() {
+		$hooks = $this->createMock( Hooks::class );
+		ServiceFactory::set_custom_hooks( $hooks );
+		$taxonomies = $this->createMock( Taxonomies::class );
+		ServiceFactory::set_custom_taxonomies( $taxonomies );
+
+		$taxonomy = new DepartmentTaxonomy();
+
+		$taxonomies
+			->expects( self::once() )
+			->method( 'register_taxonomy' )
+			->with(
+				'taxonomy_department',
+				'creogen_contact',
+				[
+					'labels' => [
+						'name' => 'Departments',
+						'singular_name' => 'Department',
+						'search_items' => 'Search Departments',
+						'all_items' => 'All Departments',
+						'edit_item' => 'Edit Department',
+						'update_item' => 'Update Department',
+						'add_new_item' => 'Add New Department',
+						'new_item_name' => 'New Department Name',
+						'menu_name' => 'Departments',
+					],
+					'public' => true,
+					'hierarchical' => true,
+					'show_ui' => true,
+					'show_in_menu' => true,
+					'show_in_nav_menus' => false,
+					'show_tagcloud' => true,
+					'show_in_quick_edit' => true,
+					'meta_box_cb' => null,
+					'show_admin_column' => true,
+					'update_count_cb' => null,
+					'query_var' => 'taxonomy_department',
+					'rewrite' => [
+						'slug' => 'taxonomy_department',
+						'with_front' => true,
+						'hierarchical' => true,
+						'ep_mask' => 'EP_MASK',
+					],
+					'capabilities' => [
+						'manage_terms' => 'manage_categories',
+						'edit_terms' => 'manage_categories',
+						'delete_terms' => 'manage_categories',
+						'assign_terms' => 'edit_posts',
+					],
+					'sort' => true,
+				]
+			);
+		$taxonomy->register_taxonomy();
 	}
 }
 
